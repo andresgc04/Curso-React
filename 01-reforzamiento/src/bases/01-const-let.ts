0fade4e3a58342efbe48e18cf4940a7f12724d64
andresgc04 <andresgc1997@outlook.com>
2025-12-07 18:44:52 -0400

    Reforzamiento
    variabes

---
+++ 01-reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,11 @@
+const firstName:string = 'Andres';
+const lastName = 'Guerrero';
+
+let diceNumber = 5;
+diceNumber = '3';
+
+console.log(firstName, lastName);
+
+const containsLetterG =  lastName.includes('G');
+
+console.log({containsLetterG, diceNumber, firstName, lastName});
