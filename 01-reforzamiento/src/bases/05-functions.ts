0fe64fab3f96f35912c6aa50be7ff36cb4e557a7
andresgc04 <andresgc1997@outlook.com>
2026-01-05 15:50:21 -0400

    add
    funciones

---
+++ 01-reforzamiento/src/bases/05-functions.ts
@@ -0,0 +1,30 @@
+function greet(name: string): string {
+  return `Hola ${name}`;
+}
+
+const greet2 = (name: string): string => {
+  return `Hola ${name}`;
+};
+
+const message = greet("Goku");
+const message2 = greet2("Vegeta");
+
+console.log(message, message2);
+
+function getUser() {
+  return {
+    uid: "ABC-123",
+    username: "El_Papi23",
+  };
+}
+
+const getUser2 = () => {
+  return {
+    uid: "ABC-123",
+    username: "El_Big_Papi23",
+  };
+};
+
+const user = getUser();
+const user2 = getUser2();
+console.log(user, user2);
