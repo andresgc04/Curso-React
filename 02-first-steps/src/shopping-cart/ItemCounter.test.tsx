1d64ad2532a2bc84059735f0842510220793fa66
AndresGc04 <andresgc1997@outlook.com>
2026-04-26 22:39:42 -0400

    Realizando Pruebas en el componente ItemCounter

---
+++ 02-first-steps/src/shopping-cart/ItemCounter.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
+import { ItemCounter } from "./ItemCounter";
+
+describe('ItemCounter', () => {
+    test('Should render with default values', () => {
+        const name = 'Control de Nintendo';
+
+        render(<ItemCounter name={name}/>);
+        
+        expect(screen.getByText(name)).toBeDefined();
+        expect(screen.getByText(name)).not.toBeNull();
+    });
+
+    test('Should render with custom quantity', () => {
+        const name = 'Control de Nintendo';
+        const quantity = 10;
+
+        render(<ItemCounter name={name} quantity={quantity}/>);
+
+        expect(screen.getByText(quantity)).toBeDefined();
+    });
+})
