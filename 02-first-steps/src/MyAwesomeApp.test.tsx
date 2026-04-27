f0271fce09d20807fc8362ad3bf115af373fe076
AndresGc04 <andresgc1997@outlook.com>
2026-04-26 21:48:10 -0400

    Buscar elementos en el componente renderizado

--- 02-first-steps/src/MyAwesomeApp.test.tsx
+++ 02-first-steps/src/MyAwesomeApp.test.tsx
@@ -1,4 +1,4 @@
-import { describe, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 
 import { MyAwesomeApp } from "./MyAwesomeApp";
@@ -7,7 +7,29 @@ describe("MyAwesomeApp", () => {
   test("Should render firstName and lastName", () => {
     const { container } = render(<MyAwesomeApp />);
 
+    // screen.debug();
+    // console.log(container.innerHTML);
+
+    const h1 = container.querySelector('h1');
+    // expect(h1?.innerHTML).toBe('Andres');
+    expect(h1?.innerHTML).toContain('Andres');
+
+    const h3 = container.querySelector('h3');
+    expect(h3?.innerHTML).toContain('Guerrero');
+
+  });
+
+    test("Should render firstName and lastName - Screen", () => {
+    render(<MyAwesomeApp />);
     screen.debug();
     // console.log(container.innerHTML);
+
+    // const h1 = screen.getByRole('heading', {
+    //   level:1
+    // });
+
+    const h1 = screen.getByTestId('first-name-title');
+
+    expect(h1.innerHTML).toContain('Andres');
   });
 });
