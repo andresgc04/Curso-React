0e3d941364ce5815aef5429fa7d5cbed8ef165a0
AndresGc04 <andresgc1997@outlook.com>
2026-05-07 00:46:53 -0400

    Realizando Componentes Ficticios - Mock Components

--- 02-first-steps/src/FirstStepsApp.test.tsx
+++ 02-first-steps/src/FirstStepsApp.test.tsx
@@ -1,11 +1,26 @@
-import { render } from "@testing-library/react";
-import { describe, expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, test, vi } from "vitest";
 import { FirstStepsApp } from "./FirstStepsApp";
+import { ItemCounter } from "./shopping-cart/ItemCounter";
+
+vi.mock('./shopping-cart/ItemCounter', () => ({
+ItemCounter:()=><div data-testid="ItemCounter" />
+}));
 
 describe("FirstStepApp", () => {
   test("Should match snapshot", () => {
     const { container } = render(<FirstStepsApp />);
 
     expect(container).toMatchSnapshot();
+  });
+
+  test('Should render the correct number of ItemCounter components', () => {
+    render(<FirstStepsApp/>);
+
+    const itemCounters = screen.getAllByTestId('ItemCounter');
+
+    expect(itemCounters.length).toBe(3);
+
+    screen.debug();
   });
 });
